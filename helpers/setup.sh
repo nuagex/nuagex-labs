883e0d989c27838351b151ce6b8e427849a2f748
Roman Dodin <dodin.roman@gmail.com>
2019-03-27 11:21:45 +0300

    added generic setup file

---
+++ helpers/setup.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+# this file is meant to be run on a nuagex jumpbox vm
+# it will pull the CATS container and clone the nuagex-lab repo
+# to enable automatic configuration of a lab
+cd ~
+docker pull nuagepartnerprogram/cats:$1
+rm -rf ~/nuagex-labs
+git clone https://github.com/nuagex/nuagex-labs
